7b0589410f627cad000f1b3d65f538f04d24b954
KacperSokolowski <kacpersokolowski64@gmail.com>
2024-03-12 20:48:29 +0100

    Add get_announcements_links function to scraper

--- scraper.py
+++ scraper.py
@@ -1,8 +1,9 @@
 import selenium
 from selenium import webdriver
 from selenium.webdriver.common.by import By
-from selenium.webdriver.chrome.options import Options
+from selenium.common.exceptions import NoSuchElementException
 from time import sleep
+import re
 
 def initialize_otodom_scraper(sleep_length : float = 0.5) -> selenium.webdriver.chrome.webdriver.WebDriver:
     """
@@ -27,4 +28,52 @@ def initialize_otodom_scraper(sleep_length : float = 0.5) -> selenium.webdriver.
 
     return driver
     
+def get_announcements_links(
+    driver : selenium.webdriver.chrome.webdriver.WebDriver,
+    first_page_url : str,
+    sleep_length : float = 0.5) -> list:
+    """
+    Navigates through a apartments for rent website's paginated listings starting from a given URL, 
+    collecting and returning the unique links to all listed property announcements.
+
+    Args:
+    - driver (selenium.webdriver.chrome.webdriver.WebDriver): A Selenium WebDriver instance for Chrome.
+    - first_page_url (str): The URL of the first page of listings.
+    - sleep_length (float, optional): The duration in seconds to wait for dynamic content to load 
+      upon navigating to a new page and after scrolling to the bottom of the page. Defaults to 0.5.
+
+    Returns:
+    - List[str]: A list of unique URLs linking to individual rent announcements found across 
+      the paginated search results.
+    """
+
+    # Container
+    announcements_links = []
+
+    # Loop through otodom using link with prefilled search
+    i=1
+    while True:
+        site = re.sub('&page=[^&]*', f'&page={i}', first_page_url)
+        driver.get(site)
+
+        # Iter until page without new result appears
+        try:
+            driver.find_element(By.XPATH, '//div[@data-cy="no-search-results"]')
+            break
+        except NoSuchElementException:
+            i+=1
+        
+        # Go to the down of the page to load all announcements
+        sleep(sleep_length)
+        driver.execute_script("window.scrollTo(0, document.body.scrollHeight);")
+        sleep(sleep_length)
+        
+        # Get announcements links
+        ts=driver.find_elements(By.XPATH, '//a[@data-cy="listing-item-link"]')
+        for t in ts:
+            announcements_links.append(t.get_attribute('href'))
+
+    # Remove duplicates; return result
+    announcements_links = list(dict.fromkeys(announcements_links))
+    return announcements_links
 
